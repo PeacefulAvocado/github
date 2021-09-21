690415c20e3e376714abb6fe56e3eef5b75db764
PeacefulAvocado <54719329+PeacefulAvocado@users.noreply.github.com>
2021-09-21 13:11:58 +0200

    Kezdőfájlok a MakeCode projekthez

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Az oldal megnyitása itt: [https://peacefulavocado.github.io/github/](https://peacefulavocado.github.io/github/)
+
+## Használj egy bővítményt
+
+Ez a repó **bővítményként** használható a MakeCode-ban.
+
+* [https://makecode.microbit.org/](https://makecode.microbit.org/) megnyitása
+* kattints au **Új projekt**-re
+* kattints a **Bővítmények** gombra a fogaskerék menüben
+* **https://github.com/peacefulavocado/github** keresése, majd importálás
+
+## A projekt szerkesztése ![Projektbuild állapotát jelző ikon](https://github.com/peacefulavocado/github/workflows/MakeCode/badge.svg)
+
+A repó szerkesztése a MakeCode-ban.
+
+* [https://makecode.microbit.org/](https://makecode.microbit.org/) megnyitása
+* kattints az **Import**-ra, utána az **Import URL**-re
+* illeszd be a **https://github.com/peacefulavocado/github** címet és kattints az importra
+
+## Blokkok előnézete
+
+Ezen a képen az utolsó, fő ághoz tartozó verzió blokk kódja látható.
+A kép frissítése eltarthat néhány percig.
+
+![Renderelt nézet blokkokkal](https://github.com/peacefulavocado/github/raw/master/.github/makecode/blocks.png)
+
+#### Metaadatok (kereséshez, rendereléshez)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,106 @@
+basic.forever(function () {
+    for (let index = 0; index <= 5; index++) {
+        basic.pause(100)
+        for (let index1 = 0; index1 <= 5; index1++) {
+            led.plot(index, index1)
+            led.unplot(index - 1, index1 - 1)
+            basic.pause(100)
+        }
+    }
+    for (let index1 = 0; index1 <= 5; index1++) {
+        basic.pause(100)
+        for (let index = 0; index <= 5; index++) {
+            led.plot(index, index1)
+            led.unplot(index - 1, index1 - 1)
+            basic.pause(100)
+        }
+    }
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . # .
+        . . # . .
+        . # . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . # . .
+        . # . . .
+        # . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # .
+        . . # . .
+        . # . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+})
